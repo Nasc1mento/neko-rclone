3a670dfa60c2b6cd8b008d5064be9968aac2df8a
Tesslon <carlos.ramos.7@proton.me>
2025-10-18 14:21:37 -0300

    chore: small changes on echo messages

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 CONFIG_DIR="$HOME/.config/neko-rclone"
-HOST_IP=$(ip route get 1 | awk '{ print $7; exit }')
+HOST_IP="$(ip route get 1 | awk '{ print $7; exit }')"
 
 echo "CONFIG_DIR=$CONFIG_DIR" > .env
 echo "Config dir at: $CONFIG_DIR"
 echo "HOST_IP=$HOST_IP" >> .env
-echo "Host IP detected: $HOST_IP"
+echo "Host IP Detected: $HOST_IP"
 
 echo "Starting containers..."
 podman-compose up -d
 ./wait-for-it.sh localhost:8080 -t 60 >/dev/null 2>&1
-echo "Neko is online at http://localhost:8080"
+echo "Neko is online at: http://localhost:8080"
 
 echo "Starting Rclone config..."
 podman exec -it neko-rclone rclone config
-echo "Complete. After rclone configuration done, "
+echo "Complete. rclone configuration done!"
